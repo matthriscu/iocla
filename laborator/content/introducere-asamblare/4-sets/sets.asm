0fda583b7059e796c1b245e89181149794c76514
Matt <matei.hriscu@protonmail.com>
2022-04-07 13:36:40 +0300

    Solved lab 5

--- laborator/content/introducere-asamblare/4-sets/sets.asm
+++ laborator/content/introducere-asamblare/4-sets/sets.asm
@@ -1,33 +1,65 @@
 %include "printf32.asm"
 
 section .text
-    global main
-    extern printf
+	global main
+	extern printf
 
 main:
-    ;cele doua multimi se gasesc in eax si ebx
-    mov eax, 139
-    mov ebx, 169
-    PRINTF32 `%u\n\x0`, eax ; afiseaza prima multime
-    PRINTF32 `%u\n\x0`, ebx ; afiseaza cea de-a doua multime
+	;cele doua multimi se gasesc in eax si ebx
+	mov eax, 139 ; 7, 3, 1, 0
+	mov ebx, 169 ; 7, 5, 3, 0
+	PRINTF32 `%u\n\x0`, eax ; afiseaza prima multime
+	PRINTF32 `%u\n\x0`, ebx ; afiseaza cea de-a doua multime
 
-    ; TODO1: reuniunea a doua multimi
+	; TODO1: reuniunea a doua multimi
 
+	mov edx, eax
+	or edx, ebx
+	PRINTF32 `%u\n\x0`, edx ; afiseaza reuniunea
 
-    ; TODO2: adaugarea unui element in multime
+	; TODO2: adaugarea unui element in multime
 
+	mov edx, 1
+;	shl edx, 5
+	or eax, edx ; adauga 5 in multimea a
 
-    ; TODO3: intersectia a doua multimi
+	mov edx, 1
+;	shl edx, 2
+	or ebx, edx ; adauga 2 in multimea b
 
+	PRINTF32 `%u\n\x0`, eax ; afiseaza prima multime
+	PRINTF32 `%u\n\x0`, ebx ; afiseaza cea de-a doua multime
 
-    ; TODO4: complementul unei multimi
+	; TODO3: intersectia a doua multimi
 
+	mov edx, eax
+	and edx, ebx
 
-    ; TODO5: eliminarea unui element
+	PRINTF32 `%u\n\x0`, edx ; afiseaza intersectia
 
+	; TODO4: complementul unei multimi
 
-    ; TODO6: diferenta de multimi EAX-EBX
+	mov edx, eax
+	not edx
 
+	PRINTF32 `%u\n\x0`, edx ; afiseaza complementul
 
-    xor eax, eax
-    ret
+	; TODO5: eliminarea unui element
+
+	mov edx, 1
+	shl edx, 5
+	not edx
+	and eax, edx ; elimina 5 din multimea a
+
+	PRINTF32 `%u\n\x0`, eax ; afiseaza a
+
+	; TODO6: diferenta de multimi EAX-EBX
+
+	mov edx, ebx
+	not edx
+	and edx, eax
+
+	PRINTF32 `%u\n\x0`, eax ; afiseaza a-b
+
+	xor eax, eax
+	ret
--- laborator/content/introducere-asamblare/5-min/min.asm
+++ laborator/content/introducere-asamblare/5-min/min.asm
@@ -8,7 +8,12 @@ main:
     ;cele doua numere se gasesc in eax si ebx
     mov eax, 4
     mov ebx, 1
-    ; TODO: aflati minimul
-    PRINTF32 `%d\n\x0`, eax ; afiseaza minimul
 
+    cmp eax, ebx
+    ; TODO: aflati minimul
+    jl print
+    mov eax, ebx
+
+print:
+    PRINTF32 `%d\n\x0`, eax ; afiseaza minimul
     ret
